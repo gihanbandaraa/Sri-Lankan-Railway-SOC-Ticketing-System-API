2e7d8fdec71921077b14a5927e1404c800df73f2
Gihan Bandara <63912749+Gihan1329@users.noreply.github.com>
2024-04-19 10:16:14 +0530

    Update Master

--- TrainAPI/Program.cs
+++ TrainAPI/Program.cs
@@ -46,11 +46,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.UseCors("AllowAll");
+
 
 app.Run();
